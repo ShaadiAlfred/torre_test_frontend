b43f3753b184eb6d4608a7171cef0bd880dc2c87
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-25 15:42:52 +0200

    Remove unused code

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,17 @@
-export interface Todo {
-  id: number;
-  content: string;
-}
-
-export interface Meta {
-  totalCount: number;
+export interface UserInfo {
+  person: {
+    name: string;
+    picture: string;
+    professionalHeadline: string;
+    summaryOfBio: string;
+    publicId: string;
+  };
+  strengths: [
+    {
+      id: string;
+      code: number;
+      name: string;
+      proficiency: string;
+    },
+  ];
 }
