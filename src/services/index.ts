dd39ce313ba34def0b03c83abc8a48a435975f98
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-25 18:25:51 +0200

    Add service to fetch user details

---
+++ src/services/index.ts
@@ -0,0 +1,17 @@
+import { UserInfo } from "../types/UserInfo";
+
+export const getUserDetails = async (
+  username: string,
+): Promise<UserInfo | null> => {
+  const url = (process.env.API_URL ?? "") + "/api/" + username + "/skills";
+
+  try {
+    const response = await fetch(url);
+
+    if (response.status === 404) throw new Error();
+
+    return (await response.json()) as unknown as UserInfo;
+  } catch (error) {
+    return null;
+  }
+};
