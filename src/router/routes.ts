86bfd849c92815c7e4cbd9799fdc3539eadf2aab
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-25 18:24:11 +0200

    Add route for to find user

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,13 +4,17 @@ const routes: RouteRecordRaw[] = [
   {
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/Index.vue") }],
+    children: [
+      { path: "", component: () => import("pages/Index.vue") },
+      { path: ":username", component: () => import("pages/UserDetails.vue") },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: "/:catchAll(.*)*",
+    name: "not_found",
     component: () => import("pages/Error404.vue"),
   },
 ];
