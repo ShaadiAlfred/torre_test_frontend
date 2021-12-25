7da8b479a31554823660ea490b146be046da7bad
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-12-25 18:25:15 +0200

    Add form to receive username

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,28 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="row">
+    <div class="col">
+      <h2 class="text-center">Enter username</h2>
+    </div>
+  </div>
+
+  <div class="row justify-center">
+    <div class="col-5 q-mx-auto">
+      <q-form
+        @submit.prevent="search"
+        autocorrect="off"
+        autocapitalize="off"
+        autocomplete="off"
+        spellcheck="false"
+        class="q-gutter-md"
+      >
+        <q-input filled v-model.trim="username" autofocus />
+
+        <div class="row justify-center">
+          <q-btn label="Submit" type="submit" color="primary" />
+        </div>
+      </q-form>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,5 +30,13 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "PageIndex",
+  data: () => ({
+    username: "",
+  }),
+  methods: {
+    async search() {
+      await this.$router.push({ path: this.username });
+    },
+  },
 });
 </script>
